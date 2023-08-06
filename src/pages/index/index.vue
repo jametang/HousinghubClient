ed64012336018087fdaa32b4c8df008a2d612ea5
tangli <18782994956@163.com>
2023-08-07 01:18:00 +0800

    add vant lib

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,35 +1,8 @@
 <template>
-  <div @click="clickHandle">
+  <div>
+    <van-search  v-model="searchstring" use-action-slot @change="onChange"  @search="onSearch" @cancel="onCancel" placeholder="请输入搜索关键词" />
+    <van-button  @click="clickHandle" type="primary">主要按钮</van-button>
 
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
   </div>
 </template>
 
@@ -43,7 +16,8 @@ export default {
       userInfo: {
         nickName: 'mpvue',
         avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      },
+      searchstring: 'hellow'
     }
   },
 
@@ -63,7 +37,19 @@ export default {
     clickHandle (ev) {
       console.log('clickHandle:', ev)
       // throw {message: 'custom test'}
-    }
+    },
+    onChange(e) {
+      this.searchstring = e.detail
+      console.log('onChange: ', this.searchstring)
+    },
+    onSearch() {
+      console.log('onSearch: ', this.searchstring)
+    },
+    onCancel() {
+      console.log('onClick: ', this.searchstring)
+    },
+
+    
   },
 
   created () {
